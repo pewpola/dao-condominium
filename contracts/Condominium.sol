48efb18efe7ce62d093de396ecf3e88a674070ee
EDUARDO Jorge Andrade Mourao Oliveira B901599 <b901599@bnb.gov.br>
2024-06-11 13:48:48 -0300

    first attributes and constructor

--- contracts/Condominium.sol
+++ contracts/Condominium.sol
@@ -2,5 +2,23 @@
 pragma solidity ^0.8.24;
 
 contract Condominium {
-    
+    address public manager; // Ownable
+    mapping(uint16 => bool) public residences; // unidade => true
+    mapping(address => uint16) public residents; // wallet => unidade (1001) -> (2505)
+    mapping(address => bool) public counselors; // conselheiro => true
+
+    constructor() {
+        manager = msg.sender;
+
+        for (uint8 i = 1; i <= 2; i++) {
+            // blocos
+            for (uint8 j = 1; j <= 5; j++) {
+                // apartamentos
+                for (uint8 k = 1; k <= 5; k++) {
+                    // unidades
+                    residences[(i * 1000) + (j * 100) + k] = true;
+                }
+            }
+        }
+    }
 }
