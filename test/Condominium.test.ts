dd08d6051238ae6cc2e11b062f9fd065b8e8096d
EDUARDO Jorge Andrade Mourao Oliveira B901599 <b901599@bnb.gov.br>
2024-06-12 08:22:06 -0300

    testing residentExists function

---
+++ test/Condominium.test.ts
@@ -0,0 +1,27 @@
+import { loadFixture } from "@nomicfoundation/hardhat-toolbox/network-helpers";
+import { expect } from "chai";
+import hre from "hardhat";
+
+describe("Condominium", function () {
+  async function deployFixture() {
+    const [manager, resident] = await hre.ethers.getSigners();
+
+    const Condominium = await hre.ethers.getContractFactory("Condominium");
+    const contract = await Condominium.deploy();
+
+    return { contract, manager, resident };
+  }
+
+  it("Should be a residence", async function () {
+    const { contract, manager, resident } = await loadFixture(deployFixture);
+
+    expect(await contract.residenceExists(2102)).to.equal(true);
+  });
+
+  // it("Should be a resident", async function () {
+  //   const { contract, manager, resident } = await loadFixture(deployFixture);
+
+  //   expect(await contract.isResident(resident.address)).to.equal(false);
+  // });
+
+});
