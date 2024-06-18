7fd866fc103fc85a40365561c80e48d35cb1bfca
EDUARDO Jorge Andrade Mourao Oliveira B901599 <b901599@bnb.gov.br>
2024-06-18 12:07:28 -0300

    aligning

--- test/Condominium.test.ts
+++ test/Condominium.test.ts
@@ -31,13 +31,15 @@ describe("Condominium", function () {
 
     const instance = contract.connect(resident);
     
-    await expect(instance.addResident(resident.address, 2102)).to.be.revertedWith("Only the manager or the council can do this");
+    await expect(instance.addResident(resident.address, 2102))
+    .to.be.revertedWith("Only the manager or the council can do this");
   });
 
   it("Should NOT add a resident (residence does not exist)", async function () {
     const { contract, manager, resident } = await loadFixture(deployFixture);
 
-    await expect(contract.addResident(resident.address, 3000)).to.be.revertedWith("This residence does not exist");
+    await expect(contract.addResident(resident.address, 3000))
+    .to.be.revertedWith("This residence does not exist");
   });
 
   it("Should remove a resident", async function () {
@@ -55,7 +57,8 @@ describe("Condominium", function () {
     const instance = contract.connect(resident);
     await contract.addResident(resident.address, 2102);
 
-    await expect(instance.removeResident(resident.address)).to.be.revertedWith("Only the manager can do this");
+    await expect(instance.removeResident(resident.address))
+    .to.be.revertedWith("Only the manager can do this");
   });
 
   it("Should NOT remove a resident (counselor cannot be removed)", async function () {
@@ -64,7 +67,8 @@ describe("Condominium", function () {
     await contract.addResident(resident.address, 2102);
     await contract.setCounselor(resident.address, true);
 
-    await expect(contract.removeResident(resident.address)).to.be.revertedWith("A counselor cannot be removed");
+    await expect(contract.removeResident(resident.address))
+    .to.be.revertedWith("A counselor cannot be removed");
   });
 
   it("Should set counselor", async function () {
@@ -82,13 +86,15 @@ describe("Condominium", function () {
     const instance = contract.connect(resident);
     await contract.addResident(resident.address, 2102);
 
-    await expect(instance.setCounselor(resident.address, true)).to.be.revertedWith("Only the manager can do this");
+    await expect(instance.setCounselor(resident.address, true))
+    .to.be.revertedWith("Only the manager can do this");
   });
 
   it("Should NOT set counselor (not a resident)", async function () {
     const { contract, manager, resident } = await loadFixture(deployFixture);
 
-    await expect(contract.setCounselor(resident.address, true)).to.be.revertedWith("The counselor must be a resident");
+    await expect(contract.setCounselor(resident.address, true))
+    .to.be.revertedWith("The counselor must be a resident");
   });
 
   it("Should set counselor (leaving)", async function () {
@@ -114,13 +120,15 @@ describe("Condominium", function () {
 
     const instance = contract.connect(resident);
 
-    await expect(instance.setManager(resident.address)).to.be.revertedWith("Only the manager can do this");
+    await expect(instance.setManager(resident.address))
+    .to.be.revertedWith("Only the manager can do this");
   });
 
   it("Should NOT set manager (invalid address)", async function () {
     const { contract, manager, resident } = await loadFixture(deployFixture);
 
-    await expect(contract.setManager("0x0000000000000000000000000000000000000000")).to.be.revertedWith("The address must be valid");
+    await expect(contract.setManager("0x0000000000000000000000000000000000000000"))
+    .to.be.revertedWith("The address must be valid");
   });
 
 });
