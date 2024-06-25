a74ffc2732fe0f31d0bdd10a11560ef8bf74f64e
EDUARDO Jorge Andrade Mourao Oliveira B901599 <b901599@bnb.gov.br>
2024-06-25 11:17:11 -0300

    created Contract Adapter

---
+++ contracts/CondominiumAdapter.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "./ICondominium.sol";
+
+contract CondominiumAdapter {
+    ICondominium private implementation;
+    address public immutable owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function upgrade(address newImplementation) external {
+        require(msg.sender == owner, "You do not have permission");
+        implementation = ICondominium(newImplementation);
+    }
+
+    function addResident(address resident, uint16 residenceId) external {
+        return implementation.addResident(resident, residenceId);
+    }
+
+    function removeResident(address resident) external {
+        return implementation.removeResident(resident);
+    }
+
+    function setCounselor(address resident, bool isEntering) external {
+        return implementation.setCounselor(resident, isEntering);
+    }
+
+    function setManager(address newManager) external {
+        return implementation.setManager(newManager);
+    }
+
+    function addTopic(string memory title, string memory description) external {
+        return implementation.addTopic(title, description);
+    }
+
+    function removeTopic(string memory title) external {
+        return implementation.removeTopic(title);
+    }
+
+    function openVoting(string memory title) external {
+        return implementation.openVoting(title);
+    }
+
+    function vote(string memory title, Lib.Options option) external {
+        return implementation.vote(title, option);
+    }
+
+    function closeVoting(string memory title) external {
+        return implementation.closeVoting(title);
+    }
+}
