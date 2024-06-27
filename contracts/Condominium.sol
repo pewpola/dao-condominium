b05aa9e0c4d34b4f3eaef2e5571b8b2ec2ddbf16
EDUARDO Jorge Andrade Mourao Oliveira B901599 <b901599@bnb.gov.br>
2024-06-27 09:59:03 -0300

    fixing the algorithm for residences creation

--- contracts/Condominium.sol
+++ contracts/Condominium.sol
@@ -16,11 +16,11 @@ contract Condominium is ICondominium {
     constructor() {
         manager = msg.sender;
 
-        for (uint8 i = 1; i <= 2; i++) {
+        for (uint16 i = 1; i <= 2; i++) {
             // blocos
-            for (uint8 j = 1; j <= 5; j++) {
+            for (uint16 j = 1; j <= 5; j++) {
                 // apartamentos
-                for (uint8 k = 1; k <= 5; k++) {
+                for (uint16 k = 1; k <= 5; k++) {
                     // unidades
                     unchecked {
                         residences[(i * 1000) + (j * 100) + k] = true;
@@ -35,13 +35,13 @@ contract Condominium is ICondominium {
     }
 
     modifier onlyManager() {
-        require(msg.sender == manager, "Only the manager can do this");
+        require(tx.origin == manager, "Only the manager can do this");
         _;
     }
 
     modifier onlyCouncil() {
         require(
-            msg.sender == manager || counselors[msg.sender],
+            tx.origin == manager || counselors[tx.origin],
             "Only the manager or the council can do this"
         );
         _;
@@ -49,7 +49,7 @@ contract Condominium is ICondominium {
 
     modifier onlyResidents() {
         require(
-            msg.sender == manager || isResident(msg.sender),
+            tx.origin == manager || isResident(tx.origin),
             "Only the manager or the residents can do this"
         );
         _;
@@ -150,7 +150,7 @@ contract Condominium is ICondominium {
             "Only VOTING topics can be voted"
         );
 
-        uint16 residence = residents[msg.sender];
+        uint16 residence = residents[tx.origin];
         bytes32 topicId = keccak256(bytes(title));
 
         Lib.Vote[] memory votes = votings[topicId];
@@ -162,7 +162,7 @@ contract Condominium is ICondominium {
         }
 
         Lib.Vote memory newVote = Lib.Vote({
-            resident: msg.sender,
+            resident: tx.origin,
             residence: residence,
             option: option,
             timestamp: block.timestamp
