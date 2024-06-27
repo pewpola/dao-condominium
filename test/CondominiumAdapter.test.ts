a50ad40a84e0222e4e8a93d31c8bc3b4361c5932
EDUARDO Jorge Andrade Mourao Oliveira B901599 <b901599@bnb.gov.br>
2024-06-27 09:07:03 -0300

    tests for upgrade function

---
+++ test/CondominiumAdapter.test.ts
@@ -0,0 +1,60 @@
+import { loadFixture } from "@nomicfoundation/hardhat-toolbox/network-helpers";
+import { expect } from "chai";
+import hre from "hardhat";
+
+describe("Condominium", function () {
+
+  enum Options {
+    EMPTY = 0,
+    YES = 1,
+    NO = 2,
+    ABSTENTION = 3
+  }
+
+  enum Status {
+    IDLE = 0,
+    VOTING = 1,
+    APPROVED = 2,
+    DENIED = 3
+  }
+
+  async function deployAdapterFixture() {
+    const accounts = await hre.ethers.getSigners();
+    const manager = accounts[0];
+
+    const CondominiumAdapter = await hre.ethers.getContractFactory("CondominiumAdapter");
+    const adapter = await CondominiumAdapter.deploy();
+
+    return { adapter, manager, accounts };
+  }
+
+  async function deployImplementationFixture() {
+    const Condominium = await hre.ethers.getContractFactory("Condominium");
+    const contract = await Condominium.deploy();
+
+    return { contract };
+  }
+
+  it("Should upgrade", async function () {
+    const { adapter, manager, accounts } = await loadFixture(deployAdapterFixture);
+    const { contract } = await loadFixture(deployImplementationFixture);
+
+    const contractAddress = await contract.getAddress();
+    await adapter.upgrade(contractAddress);
+    const address = await adapter.getImplementationAddress();
+
+    expect(address).to.equal(contractAddress);
+  });
+
+  it("Should NOT upgrade (permission)", async function () {
+    const { adapter, manager, accounts } = await loadFixture(deployAdapterFixture);
+    const { contract } = await loadFixture(deployImplementationFixture);
+
+    const contractAddress = await contract.getAddress();
+    const instance = adapter.connect(accounts[1]);
+
+    await expect(instance.upgrade(contractAddress))
+    .to.be.revertedWith("You do not have permission");
+  });
+
+});
